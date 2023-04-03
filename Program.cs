6578b2272a0859a4e96f53c0358822426fdb01dc
fishman123456 <den9152@yandex.ru>
2023-04-03 22:54:47 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine((10D).GetType());
+Console.WriteLine((10f).GetType());
+Console.WriteLine((10m).GetType());
+Console.WriteLine((0xFF).GetType());
