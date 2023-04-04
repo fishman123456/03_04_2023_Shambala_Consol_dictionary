9d2e57ea83cc66de485b3cd1652ea0e917228b87
fishman123456 <den9152@yandex.ru>
2023-04-04 07:47:37 +0700

    класс готов, с обьектом не понятно

---
+++ DictionaryKeyValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_04_2023_Shambala_Consol_dictionary
+{
+    public class DictionaryKeyValue
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        DictionaryKeyValue() { }
+        DictionaryKeyValue(string key, string value)
+        {
+           this.Key = key;
+           this.Value = value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine((10D).GetType());
-Console.WriteLine((10f).GetType());
-Console.WriteLine((10m).GetType());
-Console.WriteLine((0xFF).GetType());
+// дата создания 04-04-2023
+// словарь.
+// три класса
+
+//1 Класс (DictionaryKeyValue) - обьект слово перевод. В дальнейшем нагет надо загрузить от яндекса
+// поля - один ключ и несколько значений
+//2 Класс (SerrToXml) - принимает обьект и сохраняет его в xml, потом считывает его оттуда
+// принимает обьект и сохраняет\считывает его из xml файла
+//3 Класс (Manager) - интерфейс работы со словарем, swich и прочее
+// обработка нажатия клавиш и выбор выполняемой функции
+
+using _03_04_2023_Shambala_Consol_dictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DictionaryKeyValue one;
+        one ("red", "красный");
+        Console.WriteLine();
+    }
+
+}
